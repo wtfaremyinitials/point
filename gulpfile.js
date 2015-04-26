00a5e65f4c1bccf67f828709957c7fa6703b3650
Will Franzen <wtfranzen@gmail.com>
2015-04-25 20:51:44 -0500

    Build system

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify   = require('babelify');
 
+var less   = require('gulp-less');
+var mincss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+var concat = require('gulp-concat');
+
 gulp.task('www', function() {
     gulp
       .src('www/**')
@@ -18,9 +24,20 @@ gulp.task('scripts', function() {
         .pipe(require('fs').createWriteStream('./dist/app.js'));
 });
 
-gulp.task('default', ['scripts', 'www']);
+gulp.task('style', function() {
+    gulp
+      .src('style/*.less')
+      .pipe(less())
+      .pipe(mincss())
+      .pipe(rename(function(path) { path.extname = '.min' + path.extname }))
+      .pipe(concat('style.css'))
+      .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['scripts', 'www', 'style']);
 
 gulp.task('watch', function() {
-    gulp.watch('src/', ['scripts']);
-    gulp.watch('www/', ['www']);
+    gulp.watch('src/',   ['scripts']);
+    gulp.watch('www/',   ['www']);
+    gulp.watch('style/', ['style']);
 });
